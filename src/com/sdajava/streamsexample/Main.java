fb8f8273e6059768fbcbd61c092afe14b5ae86ea
Marciocha <taleja555@interia.pl>
2017-03-04 12:46:59 +0100

    add final files

---
+++ src/com/sdajava/streamsexample/Main.java
@@ -0,0 +1,10 @@
+package com.sdajava.streamsexample;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        List<String> words = Arrays.asList("hej")
+    }
+}
